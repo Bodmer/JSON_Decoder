6bc1e31c4f4266f60efc9275ef30563f64fdbdf0
Bodmer <bodmer@anola.net>
2018-11-02 19:30:17 +0000

    Add new Space Station example

---
+++ examples/Space_Station/ISS_API_Class.h
@@ -0,0 +1,71 @@
+// Header for Space Station class which supports the main sketch
+
+#define SHOW_JSON  // Print the JSON to the serial monitor
+#define PASSES 7   // Number of ISS passes to fetch
+
+// Choose the WiFi library to load
+#ifdef ESP8266
+  #include <ESP8266WiFi.h> // Built in library for ESP8266
+#else // ESP32
+  #include <WiFi.h>        // Built in library for ESP32
+#endif
+
+#include <WiFiUdp.h>       // Built in library
+
+#include <JSON_Decoder.h>  // Decoder library https://github.com/Bodmer/JSON_Decoder
+
+#include <JSON_Listener.h> // This is part of the JSON_Decoder library
+
+// Structure to hold the parsed values
+typedef struct ISS_pass {
+  String    message;       // Message, usually "sucess"
+  uint32_t  datetime = 0;  // Time of request, UTC unix time in seconds since Jan 01 1970
+  uint16_t  passDuration[PASSES] = { 0 }; // Pass duration in seconds
+  uint32_t  passRiseTime[PASSES] = { 0 }; // Time of pass, UTC unix time
+} ISS_pass;
+
+class SpaceStation: public JsonListener {
+
+  public:
+
+    void getPasses(String latitude, String longitude, ISS_pass* passData);
+
+    // Start of Listener support functions
+    void key(const char *key);
+
+    void value(const char *value);
+
+    void startDocument();
+
+    void endDocument();
+
+    void startObject();
+
+    void endObject();
+
+    void startArray();
+
+    void endArray();
+
+    // void whitespace(char c); // No need to include functions if not used
+
+    void error( const char *message );
+    // End of Listener support functions
+
+  private:
+
+    bool     ended = true;  // Flag to indicate document has ended
+
+    String   currentParent; // Current object e.g. "daily"
+
+    String   currentKey;    // Name key of the name:value pair e.g "temperature"
+
+    uint16_t arrayIndex;    // Array index e.g. 5 for day 5 forecast, qualify with arrayPath
+
+    String   valuePath;     // object (i.e. sequential key) path (like a "file path")
+                            // taken to the name:value pair in the form "hourly/data"
+                            // so values can be pulled from the correct array.
+                            // Needed since different objects contain "data" arrays.
+
+    ISS_pass *passData;     // pointer provided by sketch
+};
